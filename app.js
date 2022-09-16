962696fabeba7697dd57c0fe819440906b13f474
Lorenzo Spoleti <LorenzoSpoleti@outlook.com>
2022-09-16 22:11:36 +0200

    *fixed returned name and formatting

--- app.js
+++ app.js
@@ -214,7 +214,7 @@ app.get('/get_random_place', async (req, res) => {
 
   details.formatted_address = geocodeResult[placeIndex].formatted_address
   details.geonameid = place[0].geonameid
-  details.name = place[0].name
+  details.name = geocodeResult[placeIndex].formatted_address.split(',')[0].replace(/[0-9]/g, '')
   details["country code"] = place[0]["country code"]
   res.send(details)
   return;
